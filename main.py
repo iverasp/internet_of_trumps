df86802dff363210f94906b163a73d410de90b25
Iver Egge <iverasp@gmail.com>
2016-10-11 19:12:48 +0200

    say random trump quote

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from gtts import gTTS
+import random
+from playsound import playsound
+
+
+trump = True
+
+def get_quotes():
+    quotes = open("quotes.txt").read().split(";")
+    return quotes
+
+def say_random_quote(quotes):
+    quote = random.choice(quotes)
+    tts = gTTS(text=quote,lang='en')
+    tts.save("quote.mp3")
+    playsound("quote.mp3")
+
+def main():
+    quotes = get_quotes()
+    say_random_quote(quotes)
+
+if __name__ == "__main__":
+    main()
